79b04f0b77ef89b6df916e04c39cfa60c9535aba
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-06-13 21:34:33 +0500

    added new code

---
+++ client/src/components/Username.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Username() {
+  return (
+    <div className="container mx-auto">
+        <div className='flex justify-center items-center h-screen'>
+            <div>
+                <div className='title flex flex-col items-center'>
+                    <h4 className='text-5xl font-bold'>Hello Again</h4>
+                    <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+                        Explore More by connecting with us
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
