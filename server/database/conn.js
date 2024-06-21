f4c3b5b4ebbd07fdbb9fec8497e0b1959dcf61c4
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-06-21 11:04:57 +0500

    added route.js code

--- server/database/conn.js
+++ server/database/conn.js
@@ -2,14 +2,17 @@ import mongoose from "mongoose";
 
 import { MongoMemoryServer } from "mongodb-memory-server";
 
+
 async function connect(){
+
     const mongod = await MongoMemoryServer.create();
     const getUri = mongod.getUri();
 
-    mongoose.set("strictQuary", true)
-    const db = await mongoose.connect(getUri);
-    console.log("Database Connected");
-    return db;    
+    mongoose.set('strictQuery', true)
+    // const db = await mongoose.connect(getUri);
+    const db = await mongoose.connect(ENV.ATLAS_URI);
+    console.log("Database Connected")
+    return db;
 }
 
 export default connect;
--- server/router/route.js
+++ server/router/route.js
@@ -1,2 +1,10 @@
 import { Router } from "express";
 const router = Router();
+
+// POST Methods
+router.route('/register').post((req, res) => res.json('register route'));
+// GET Methods
+
+// PUT Methods
+
+export default router;
